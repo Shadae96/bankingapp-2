84ede77126d77e9ddf3c88e6d4c6797e2507db10
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-08-14 17:06:46 -0500

    worked on connection Dao

--- bankingapp2/src/main/java/com/example/dao/UserDaoJavaImpl.java
+++ bankingapp2/src/main/java/com/example/dao/UserDaoJavaImpl.java
@@ -3,44 +3,104 @@ package com.example.dao;
 
 import com.example.models.User;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-import com.example.models.Customer;
-import com.example.models.Employee;
-
-import com.example.store.Store;
-
 public class UserDaoJavaImpl implements UserDao {
+    String url = "jdbc:postgresql:// my-database.cr9ubtmwagwv.us-east-2.rds.amazonaws.com/bankingapp";
+    String username = "postgres";
+    String password = "Shia8645!";
+
+
 
 @Override 
 public List <User> getAllUsers(){
-    return Store.users;
+
+    List <User> users = new ArrayList<>();
+
+    try {
+        Connection conn = DriverManager.getConnection(url, username,password);
+
+        String sql = "select * from users";
+
+        PreparedStatement ps = conn.prepareStatement(sql);
+
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()) {
+            users.add(new User(rs.getInt(1),rs.getString(2), rs.getString(3),rs.getString(4),rs.getString(5)));
+
+        }
+
+        conn.close();
+     
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+    return null;
 }
     
+@Override public User getUserByUsername(String username) {
+    
+    User user = null;
+    
+    //try with resources will close the object thats within the parenthesis
+    try {
+        Connection conn = DriverManager.getConnection(url, username,password);
+
+        //sql that we will be executing
+        String sql = "select * from users where username = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+
+        ps.setString(1, username);
+
+        //execute the SQL statement and return the result set into the variable rs
+        ResultSet rs = ps.executeQuery();
+
+        //iterate through the result set
+        while(rs.next()){
+            user = new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+        }
+        
+        conn.close();
+
+    } catch (SQLException e) {
+
+    }
+
+    return user;
+}
 
 
 @Override
- public User getUserByUsername(String username){
-
-    User userFromDb = null;
-
-    for (User user : Store.users){
-        if (user == null);
-            break;
-    }
-        if(user.getUsername().equals(username)){
-            userFromDb = user;
-            break;
-        }
-        return userFromDb;
- }      
+public void createUser(User user) {
     
+    try {
+        Connection conn = DriverManager.getConnection(url, username,password);
 
+        //sql that we will be executing
+        String sql = "insert into users (username, password, firstname, lastname) values (?,?,?,?);";
+        PreparedStatement ps = conn.prepareStatement(sql);
 
+        //the parameterIndex is references what questionmark we want to put a value in for
+        ps.setString(1, user.getUsername());
+        ps.setString(2, user.getPassword());
+        ps.setString(3, user.getFirstname());
+        ps.setString(4, user.getLastname());
 
-@Override   
-public void createUser (User user) {
-    Store.users.add(user);
+        ps.executeUpdate();
+
+        conn.close();
+        
+    } catch (SQLException e) {
+    
+    }
 }
-}
 
+}
--- bankingapp2/src/main/java/com/example/util/InputUtil.java
+++ bankingapp2/src/main/java/com/example/util/InputUtil.java
@@ -12,10 +12,25 @@ public class InputUtil {
         return scanner.nextLine();
     }
 
-    public Integer retrieveInt(String msg){
+public Integer retrieveInt(String msg) {
         
+        Integer num = 0;
+        Boolean valid = false;
+        while (!valid) {
+            System.out.print(msg);
+            try {
+                num = Integer.parseInt(scanner.nextLine());
+                //throw new RuntimeException();  //this is how we manually throw an exception
+                valid = true;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Not a number...");
+            } catch (Exception e){
+                e.printStackTrace();
+            }  finally{
+                //this will run if an exception happens or not
+            }
+        }
 
-
+        return num;
     }
-    
 }
