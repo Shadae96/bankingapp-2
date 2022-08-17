05d6f94b8e961a6790f2440925f3ea1886676b37
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-08-17 07:12:49 -0500

    worked on accounts dao and models

---
+++ bankingapp2/src/main/java/com/example/models/Employee.java
@@ -0,0 +1,12 @@
+package com.example.models;
+
+public class Employee extends User {
+    public static void main(String[] args) {
+        
+        private Boolean customer = false;
+        
+        Employee newEmployee = new Employee();
+        newEmployee.getLastname();
+
+    }
+}
